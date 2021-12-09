8f7e5f6b1cf528567858507d9d011ca021735680
hatch <hatch.tech.eng@gmail.com>
2021-12-10 00:55:29 +0900

    MenuItemを分離

---
+++ Editor/ColorFolderMenuItem.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Yorozu.EditorTool.Folder
+{
+    internal class ColorFolderMenuItem
+    {
+        [MenuItem("Tools/ColorFolder/PingFolderSetting")]
+        private static void Select()
+        {
+            var findGuids = AssetDatabase.FindAssets($"t:{nameof(FolderSettingData)}");
+            if (findGuids.Length <= 0)
+                return;
+
+            var path = AssetDatabase.GUIDToAssetPath(findGuids[0]);
+
+            var data = AssetDatabase.LoadAssetAtPath<FolderSettingData>(path);
+            EditorGUIUtility.PingObject(data);
+            Selection.activeObject = data;
+        }
+        
+        /// <summary>
+        /// 設定ファイルの生成
+        /// </summary>
+        [MenuItem("Tools/ColorFolder/CreateFolderSetting")]
+        private static void Create()
+        {
+            var path =EditorUtility.SaveFilePanelInProject("Select Create Asset Path", nameof(FolderSettingData), "asset", "");
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            var asset = ScriptableObject.CreateInstance<FolderSettingData>();
+            AssetDatabase.CreateAsset(asset, path);
+            AssetDatabase.Refresh();
+        }   
+    }
+}
--- Editor/FolderSettingData.cs
+++ Editor/FolderSettingData.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -12,35 +11,6 @@ namespace Yorozu.EditorTool.Folder
     /// </summary>
     public class FolderSettingData : ScriptableObject
     {
-        [MenuItem("Tools/ColorFolder/Ping FolderSetting")]
-        private static void Select()
-        {
-            var findGuids = AssetDatabase.FindAssets($"t:{nameof(FolderSettingData)}");
-            if (findGuids.Length <= 0)
-                return;
-
-            var path = AssetDatabase.GUIDToAssetPath(findGuids[0]);
-
-            var data = AssetDatabase.LoadAssetAtPath<FolderSettingData>(path);
-            EditorGUIUtility.PingObject(data);
-            Selection.activeObject = data;
-        }
-        
-        /// <summary>
-        /// 設定ファイルの生成
-        /// </summary>
-        [MenuItem("Tools/ColorFolder/CreateFolderSetting")]
-        private static void Create()
-        {
-            var path =EditorUtility.SaveFilePanelInProject("Select Create Asset Path", nameof(FolderSettingData), "asset", "");
-            if (string.IsNullOrEmpty(path))
-                return;
-
-            var asset = CreateInstance<FolderSettingData>();
-            AssetDatabase.CreateAsset(asset, path);
-            AssetDatabase.Refresh();
-        }
-        
         [NonSerialized]
         private Dictionary<int, Texture> _cacheTexture = new Dictionary<int, Texture>();
 
